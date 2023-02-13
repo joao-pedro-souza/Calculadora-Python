64beb18cbb4bdcbabccadfdd36a952639eba2ebd
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-02-13 20:21:35 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,19 @@
 while True:
 
+    numero1 = input('Digite um número: ')
+    numero2 = input('Digite outro número: ')
+
+    numeros_validos = None
+
+    try:
+        float_numero1 = float(numero1)
+        float_numero2 = float(numero2)
+        numeros_validos = True
+    except:
+        print('Pelo menos um dos números digitados é inválido.')
+        continue
+
+
     sair = input('Deseja sair? [S]/[N]: ').lower().startswith('s')
 
     if sair:
